5801dead3f016e307abf844765baae3208eee54e
Sai Kishan <93516324+saikishan5000@users.noreply.github.com>
2023-04-08 11:26:20 +0530

    Add files via upload

--- loginSignUpwithFirebase/signin.js
+++ loginSignUpwithFirebase/signin.js
@@ -1,7 +1,11 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-analytics.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 // Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,13 +18,12 @@ const firebaseConfig = {
   projectId: "wealth-wizards5000",
   storageBucket: "wealth-wizards5000.appspot.com",
   messagingSenderId: "782499607192",
-  appId: "1:782499607192:web:1102017a58ea71700496ed"
+  appId: "1:782499607192:web:1102017a58ea71700496ed",
 };
 
 // Initialize Firebase
- const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-  
 //   console.log(app);
 
 function checkLogin() {
@@ -28,7 +31,7 @@ function checkLogin() {
 
   if (userLoggedIn) {
     // User is logged in, so redirect to the home page
-    window.location.href = '/home.html';
+    window.location.href = "/home.html";
   } else {
     // User is not logged in, so do nothing (stay on the sign-in page)
     console.log("User is not logged in.");
@@ -37,48 +40,47 @@ function checkLogin() {
 
 checkLogin();
 
+//----- New Registration code start
+const auth = getAuth(app);
 
-  //----- New Registration code start	  
-  const auth = getAuth(app);
-  
-  //----- End
+//----- End
 
-  //----- Login code start	  
-  document.getElementById("signInBtn").addEventListener("click", function() {
-      var email =  document.getElementById("email").value;
-      var password = document.getElementById("password").value;
+//----- Login code start
+document.getElementById("signInBtn").addEventListener("click", function () {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
 
-      signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        console.log(user);
-        alert(user.email+" Login successfully!!!");
-        localStorage.setItem("IsLoggedIn",true)
-        //Redirect to different page after login
-        window.location.href = '/home.html';
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage);
-        alert(errorMessage);
-      });		  		  
-  });
-  //----- End
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+      alert(user.email + " Login successfully!!!");
+      localStorage.setItem("IsLoggedIn", true);
+      //Redirect to different page after login
+      window.location.href = "/home.html";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      alert(errorMessage);
+    });
+});
+//----- End
 
-  //----- Logout code start	  
-  // document.getElementById("logout").addEventListener("click", function() {
-  //     signOut(auth).then(() => {
-  //         // Sign-out successful.
-  //         console.log('Sign-out successful.');
-  //         alert('Sign-out successful.');
-  //         document.getElementById('logout').style.display = 'none';
-  //       }).catch((error) => {
-  //         // An error happened.
-  //         console.log('An error happened.');
-  //       });		  		  
-  // });
-  //----- End
+//----- Logout code start
+// document.getElementById("logout").addEventListener("click", function() {
+//     signOut(auth).then(() => {
+//         // Sign-out successful.
+//         console.log('Sign-out successful.');
+//         alert('Sign-out successful.');
+//         document.getElementById('logout').style.display = 'none';
+//       }).catch((error) => {
+//         // An error happened.
+//         console.log('An error happened.');
+//       });
+// });
+//----- End
 
-  export default app;
+export default app;
--- loginSignUpwithFirebase/signup.js
+++ loginSignUpwithFirebase/signup.js
@@ -1,5 +1,8 @@
 // import app from "./script.js"
-import { getAuth, createUserWithEmailAndPassword,  } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 // console.log(app)
@@ -10,22 +13,21 @@ const firebaseConfig = {
   projectId: "wealth-wizards5000",
   storageBucket: "wealth-wizards5000.appspot.com",
   messagingSenderId: "782499607192",
-  appId: "1:782499607192:web:1102017a58ea71700496ed"
+  appId: "1:782499607192:web:1102017a58ea71700496ed",
 };
-  
-  // Initialize Firebase
-   const app = initializeApp(firebaseConfig);
-  console.log(app)
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app);
 
 const auth = getAuth(app);
-
 
 function checkLogin() {
   const userLoggedIn = localStorage.getItem("IsLoggedIn");
 
   if (userLoggedIn) {
     // User is logged in, so redirect to the home page
-    window.location.href = '/home.html';
+    window.location.href = "/home.html";
   } else {
     // User is not logged in
     console.log("User is not logged in.");
@@ -34,28 +36,27 @@ function checkLogin() {
 
 checkLogin();
 
-
-document.getElementById("signUpBtn").addEventListener("click", function() {
-  var email =  document.getElementById("email").value;
+document.getElementById("signUpBtn").addEventListener("click", function () {
+  var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
-  var firstName =  document.getElementById("firstName").value;
+  var firstName = document.getElementById("firstName").value;
   var lastName = document.getElementById("lastName").value;
   //For new registration
   createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user);
-    
-    alert("Registration successfully!!");
-    window.location.href = '/signIp.html';
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-    console.log(errorMessage);
-    alert(error);
-  });		  		  
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+
+      alert("Registration successfully!!");
+      window.location.href = "/home.html";
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+      console.log(errorMessage);
+      alert(error);
+    });
 });
